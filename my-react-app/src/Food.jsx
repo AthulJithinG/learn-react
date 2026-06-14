19875029791c14a5db360003713cd1fbf2611149
Athul <athulkrishnaps2001@gmail.com>
2026-06-14 19:51:36 +0530

    React Components

---
+++ my-react-app/src/Food.jsx
@@ -0,0 +1,19 @@
+
+
+function Food() {
+    
+    const food1 = "Apple";
+    const food2 = "Orange";
+    const food3 = "Banana";
+
+    return(
+        <ul>
+            <li>{food1}</li>
+            <li>{food2}</li>
+            <li>{food3.toUpperCase()}</li>
+        </ul>
+    );
+
+}
+
+export default Food
